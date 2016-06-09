731638d4972bf3f8cb3dc425034a38345be10655
VirginieKY <ky@user.io>
2016-06-09 14:24:46 +0200

    menu-collapse directive not working yet

---
+++ client/src/app/components/menu-collapse.directive.js
@@ -0,0 +1,60 @@
+/*angular.module('Transcode')
+	.directive('tcMenuColapse', ['$uibModal', function($uibModal) {
+		return {
+			restrict: 'A',
+			link: function($scope, element, attrs) {
+				
+				element.bind('click', function() {
+					if ($scope.tcConfirmIf()) {	
+						var confirmationDialog = $uibModal.open({
+		    			backdrop: false,
+		    			templateUrl: attrs.templateUrl || 'template/modal/confirmation-dialog.tpl.html',
+		    			windowTemplateUrl: attrs.windowTemplateUrl || 'template/modal/window.html',
+		    			size: 'sm'
+		    		});
+
+		    		confirmationDialog.result.then(function(isConfirmed) {
+		    			if (isConfirmed)
+		    				$scope.tcDo(); 
+						});
+		    	} else {
+		    		$scope.tcDo(); 
+		    	}
+				});
+			}
+		};
+	}]);*/
+	
+angular.module('Transcode').directive('tcMenuColapse', ['$animate',
+    function ($animate) {
+        return {
+            link: function (scope, elem, attrs) {
+            	console.log("YOOOOO");
+                elem.on('click', function () {
+                    var self = angular.element(this);
+                    $animate.addClass(self, 'spin', function () {
+                        self.removeClass('spin');
+                    });
+                });
+            }
+        };
+}]);
+	
+	
+/*angular.module('Transcode')
+	.directive('tcMenuColapse', ['$animate', function ($animate) {
+        return {
+            link: function (scope, elem, attrs) {
+                elem.on('click', function () {
+                    //var self = angular.element(this);
+                    
+                    //console.log("YO")
+                    
+                    var self = angular.element("#menu");
+                    $animate.addClass(self, 'spin', function () {
+                        self.removeClass('spin');
+                    });
+                });
+            }
+        };
+}]);*/
